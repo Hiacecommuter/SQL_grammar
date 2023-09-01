36c1d97e19efe49e3dc10bc3279d0c2d1794a9e2
Hiacecommuter <82562175+Hiacecommuter@users.noreply.github.com>
2023-09-01 21:21:28 +1000

    Create join_with_comparison.sql

---
+++ join_with_comparison.sql
@@ -0,0 +1,21 @@
+--- only joins rows that fit the investments.funded_year > companies.founded_year + 5 condition
+SELECT companies.permalink,
+       companies.name,
+       companies.status,
+       COUNT(investments.investor_permalink) AS investors
+  FROM tutorial.crunchbase_companies companies
+  LEFT JOIN tutorial.crunchbase_investments_part1 investments
+    ON companies.permalink = investments.company_permalink
+   AND investments.funded_year > companies.founded_year + 5
+ GROUP BY 1,2, 3
+
+--- joining all rows and then filtering
+SELECT companies.permalink,
+       companies.name,
+       companies.status,
+       COUNT(investments.investor_permalink) AS investors
+  FROM tutorial.crunchbase_companies companies
+  LEFT JOIN tutorial.crunchbase_investments_part1 investments
+    ON companies.permalink = investments.company_permalink
+ WHERE investments.funded_year > companies.founded_year + 5
+ GROUP BY 1,2, 3
