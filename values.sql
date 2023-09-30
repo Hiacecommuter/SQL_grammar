42698b102466393265c52c2d7918f39f86e7b9a9
Hiacecommuter <82562175+Hiacecommuter@users.noreply.github.com>
2023-09-30 15:19:20 +1000

    Update values.sql

--- values.sql
+++ values.sql
@@ -9,3 +9,6 @@ WHERE column1 = 2;
 
 INSERT INTO Customers (CustomerName, ContactName, Address, City, PostalCode, Country)
 VALUES ('Cardinal','Tom B. Erichsen','Skagen 21','Stavanger','4006','Norway');
+
+values row(6,5,3), row(3, 2, 5);
+select max(column_0) from (values row(6,5,3), row(3, 2, 5)) as tb;
